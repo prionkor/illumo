b631b1f875cf58d09366c2e62ce7dc8f4560fead
Sisir Kanti Adhikari <sisir@Sisirs-MacBook-Pro.local>
2024-12-24 15:18:00 +0600

    Set darkmode as default in storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,17 +1,27 @@
-import type { Preview } from "@storybook/angular";
-import { setCompodocJson } from "@storybook/addon-docs/angular";
-import docJson from "../documentation.json";
+import type { Preview } from '@storybook/angular';
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+import docJson from '../documentation.json';
 setCompodocJson(docJson);
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		darkMode: {
+			current: 'dark', // 👈 Set the current mode to 'dark'
+		},
+		backgrounds: {
+			default: 'dark',
+			values: [
+				{ name: 'dark', value: '#333333' }, // 👈 Define the dark background color
+				{ name: 'light', value: '#FFFFFF' }, // Optional: Define a light background for contrast
+			],
+		},
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
 };
 
 export default preview;
